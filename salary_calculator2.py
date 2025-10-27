3b00befed4420667c15bc81b5b22c30e984a9674
Iosif Papaspyros <iosif0989@gmail.com>
2025-10-27 12:38:09 +0200

    Create salary_calculator2.py

---
+++ salary_calculator2.py
@@ -0,0 +1,30 @@
+#υπολογισμος
+def computepay(h, r):
+    h = float(h)
+    r = float(r)
+    if h <= 40:
+        salary = h * r
+    else:
+        salary = (40 * r) + ((h - 40) * (r * 1.5))
+    return salary
+#tring to get the right number#
+def get_float(x):
+    while True:
+        value=input(x)
+        try:
+            value = float(value)
+            if value < 0:
+                print("Please enter a non-negative number.")
+                continue
+            return value          
+        except:
+            print("Please enter a numeric value (e.g. 40 or 7.5).")
+#main
+def calculation():
+    h = get_float("Write your working hours please: ")
+    r = get_float("Write your rate per hour please: ")
+    pay = computepay(h, r)
+    print("Pay:", pay)
+#the program
+calculation()     
+
