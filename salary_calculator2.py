64d0228bf4bfe4c975beec4a4427f4f4a0659c42
George Georgitsis <georgegeorgitsis@gmail.com>
2025-10-28 20:21:20 +0200

    Created class Calculator

--- salary_calculator2.py
+++ salary_calculator2.py
@@ -1,35 +1,8 @@
-# Calculate
-def compute_pay(h, r):
-    h = float(h)
-    r = float(r)
-    if h <= 40:
-        salary = h * r
-    else:
-        salary = (40 * r) + ((h - 40) * (r * 1.5))
-    return salary
+# This imports the Calculator class
+from calculator import Calculator
 
+# Creates an instance of the Calculator class
+calculator = Calculator()
 
-# trying to get the right number#
-def get_float(x):
-    while True:
-        value = input(x)
-        try:
-            value = float(value)
-            if value < 0:
-                print("Please enter a non-negative number.")
-                continue
-            return value
-        except:
-            print("Please enter a numeric value (e.g. 40 or 7.5).")
-
-
-# main
-def calculation():
-    h = get_float("Write your working hours please: ")
-    r = get_float("Write your rate per hour please: ")
-    pay = compute_pay(h, r)
-    print("Pay:", pay)
-
-
-# the program
-calculation()
+# Calls the calculation method of the Calculator class
+calculation = calculator.calculation()
